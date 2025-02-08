2a03cfafa1e5d92bea54b9a6af0505f7ce760520
Julian-AT <julian.schmidt1005@gmail.com>
2025-02-08 20:31:41 +0100

    style: apply dynamic height to github feed

--- app/(home)/components/feed.tsx
+++ app/(home)/components/feed.tsx
@@ -1,21 +1,20 @@
-import { GitHubEvent } from '@julian-at/app/(home)/components/event';
-import { Section } from '@julian-at/components/section';
-import { octokit } from '@julian-at/lib/github';
-import { cn } from '@julian-at/lib/utils';
-import { ViewAnimation } from '@julian-at/providers/view-animation';
+import { GitHubEvent } from "@julian-at/app/(home)/components/event";
+import { Section } from "@julian-at/components/section";
+import { octokit } from "@julian-at/lib/github";
+import { cn } from "@julian-at/lib/utils";
+import { ViewAnimation } from "@julian-at/providers/view-animation";
 
 export const Feed = async () => {
   const activity = await octokit.rest.activity.listPublicEventsForUser({
-    username: 'julian-at',
-    per_page: 100,
-    page: 1,
+    username: "julian-at",
+    per_page: 15,
   });
 
   return (
     <Section
       className={cn(
-        'relative flex min-h-48 flex-col gap-2 px-4 py-8 font-mono text-muted-foreground text-xs',
-        'sm:px-8 sm:text-sm'
+        "relative flex flex-col gap-2 px-4 py-8 font-mono text-muted-foreground text-xs",
+        "sm:px-8 sm:text-sm"
       )}
     >
       {activity.data.slice(0, 10).map((event, index) => (
